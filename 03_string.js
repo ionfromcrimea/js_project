54f72345514a431adcc73e201fb87a992b9ee152
Oleg <ionfromcrimea@gmail.com>
2020-09-10 09:57:56 +0300

    string

---
+++ 03_string.js
@@ -0,0 +1,23 @@
+// const name = 'Oleg'
+// console.log(typeof name);
+// const age = 53
+// const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
+// const output = `Привет, меня зовут ${name} и мой возраст ${age <30 ? 'мало' : 'много'} лет.`
+// console.log(output);
+
+// const output = `
+//   <div>This is div</div>
+//   <p>This is p</p>
+// `
+// console.log(output);
+
+// const name = 'Oleg' // new String
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(2));
+// console.log(name.indexOf('eg'));
+// console.log(name.indexOf('tt'));
+// console.log(name.toUpperCase().startsWith('OL'));
+// console.log(name.repeat(3));
+
+
--- app.js
+++
@@ -1,8 +0,0 @@
-const name = 'Oleg'
-console.log(typeof name);
-const age = 53
-// const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
-const output = `Привет, меня зовут ${name} и мой возраст ${age} лет.`
-console.log(output);
-
-    
